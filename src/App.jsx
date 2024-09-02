ad1d3dd5e4648d86cdc3e1d46817b2dc0aaf5cf7
M. KALEEMULLAH KHAN QURESHI <136243025+kaleemzil@users.noreply.github.com>
2024-09-02 01:23:18 -0700

    digital-cock react.js
    i am created digital clock by the help of react.js in this clock i can use react hooks and simple javascript.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DigitalClock from "./Digital-Clock.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+        <DigitalClock/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/Digital-Clock.jsx
@@ -0,0 +1,46 @@
+import { useState , useEffect} from 'react';
+
+function DigitalClock(){
+
+    const[time , setTime] = useState(new Date());
+    useEffect(()=>{
+            const intervalId = setInterval(()=>{
+                setTime(new Date());
+            },1000);
+            return()=>{
+                clearInterval(intervalId);
+            }
+    },[]);
+
+    
+
+    function formatTime(){
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+
+        let meridium = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12 ;
+
+        return `${padZero(hours)} : ${padZero(minutes)} : ${padZero(seconds)} ${meridium}` ; 
+    }
+
+    function padZero(number){
+        return ( number <  10 ? "0" : ""  ) + number;
+    }
+
+    return(
+
+        <>
+            <div className='clock-container'>
+                <div className='clock'>
+                    <span>{formatTime()}</span>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default DigitalClock;
